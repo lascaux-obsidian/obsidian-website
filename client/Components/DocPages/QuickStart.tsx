5a44c49b6fcf9e307d39d415efd7df4c5d5e4cb1
Travis Frank <travis@travismfrank.com>
2020-09-18 17:15:01 -0400

    Added codeblocks in deps

--- client/Components/DocPages/QuickStart.tsx
+++ client/Components/DocPages/QuickStart.tsx
@@ -1,5 +1,4 @@
-import { React } from '../../../deps.ts';
-
+import { React, CodeBlock, monokai, dracula } from '../../../deps.ts';
 
 declare global {
   namespace JSX {
@@ -23,22 +22,41 @@ const QuickStart = (props: any) => {
       <p>Optimized for use in server-side rendered React apps built with Deno, fullstack integration of <code>obsidian</code> enables many of its most powerful features, including optimized caching exchanges between client and server as well as server-side user session storage, maintaining the benefits of server-side rendering even after client-side caches have expired.</p>
       <h2>Installation</h2>
       <p>In the server:</p>
-      {/*<Editor
-        value={"import { ObsidianRouter } from 'https://deno.land/x/obsidian@v1.0.0/mod.ts'"}
-        highlight={(code: any) => highlight(code, languages.js)}
-        padding={10}
-        style={{
-          fontFamily: '"Fira code", "Fira Mono", monospace',
-          fontSize: 12,
-        }}
-      />*/}
+      <CodeBlock
+        text={"import { ObsidianRouter } from 'https://deno.land/x/obsidian@v1.0.0/mod.ts'"}
+        language={"jsx"}
+        showLineNumbers={false}
+        theme={monokai}
+        className="codeBlock"
+      />
       <p>In the app:</p>
-      <code>
-        <p>
-          import {"{ ObsidianWrapper }"} from 'https://deno.land/x/obsidian@v1.0.0/mod.ts'
-        </p>
-      </code>
+      <CodeBlock
+        text={"import { ObsidianWrapper } from 'https://deno.land/x/obsidian@v1.0.0/mod.ts'"}
+        language={"jsx"}
+        showLineNumbers={false}
+        theme={monokai}
+        className="codeBlock"
+      />
       <h2>Creating the Router</h2>
+      <CodeBlock
+        text={`import { ObsidianWrapper } from 'https://deno.land/x/obsidian/mod.ts'
+import { React } from 'https://dev.jspm.io/react@16.13.1';
+import { App } from 'App.tsx';
+
+class Wrapper extends React.Component {
+  render() {
+    return (
+      <ObsidianWrapper>
+        <App />
+      </ObsidianWrapper>
+    )
+  }
+}`}
+        language={"jsx"}
+        showLineNumbers={true}
+        theme={monokai}
+        className="codeBlock"
+      />
       <h2>Sending ObsidianSchema</h2>
       <h2>Creating the Wrapper</h2>
       <h2>Making a Query</h2>
