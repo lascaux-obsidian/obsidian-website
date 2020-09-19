115023522193ac4481f3a8ec6f982d917bb1f3c5
Matt Meigs <mattmeigs@gmail.com>
2020-09-18 21:09:21 -0400

    favicon added

--- server.tsx
+++ server.tsx
@@ -92,6 +92,7 @@ function handlePage(ctx: any) {
   <head>
     <meta charset="UTF-8">
     <link rel="stylesheet" href="/static/style.css">
+    <link rel="icon" type="image/png" href="/static/obsidianLogo3.png"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
