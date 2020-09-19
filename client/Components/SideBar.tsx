87a94c72fb75ef89e6e87e2f0fe8b3d1d0c314a4
Alonso Garza <alonsogarza@Alonsos-MacBook-Pro.local>
2020-09-18 19:22:50 -0500

    Fixed main page

--- client/Components/SideBar.tsx
+++ client/Components/SideBar.tsx
@@ -15,38 +15,42 @@ declare global {
   }
 }
 
-
 const NavBar = (props: any) => {
   const { page } = props;
 
   let curContext;
 
-  if (page === 'home') curContext = <MainContext />
-  if (page === 'about') curContext = <AboutContext />
-  if (page === 'demo') curContext = <DemoContext />
-  if (page === 'docs') curContext = <DocsContext />
+  if (page === 'home') curContext = <MainContext />;
+  if (page === 'about') curContext = <AboutContext />;
+  if (page === 'demo') curContext = <DemoContext />;
+  if (page === 'docs') curContext = <DocsContext />;
 
   return (
-    <div className="sidebar">
-      <div className="codeLinks">
-        <a href="https://github.com/oslabs-beta/obsidian">
-          <div className="codeLinkDiv">
-            <img id="githubLogo" src="../static/github-icon.svg" alt="GitHub Logo" />
-            <h4>GitHub</h4>
+    <div
+      className='sidebar'
+      style={page === 'home' ? { backgroundColor: 'rgba(0,0,0,0)' } : {}}
+    >
+      <div className='codeLinks'>
+        <a href='https://github.com/oslabs-beta/obsidian'>
+          <div className='codeLinkDiv'>
+            <img
+              id='githubLogo'
+              src='../static/github-icon.svg'
+              alt='GitHub Logo'
+            />
+            <h4>·GitHub</h4>
           </div>
         </a>
-        <a href="https://deno.land/x/obsidian">
-          <div className="codeLinkDiv">
-            <img src="../static/Deno-Logo.svg" alt="Deno Logo" />
-            <h4>deno.land</h4>
+        <a href='https://deno.land/x/obsidian'>
+          <div className='codeLinkDiv'>
+            <img src='../static/Deno-Logo.svg' alt='Deno Logo' />
+            <h4>·deno.land</h4>
           </div>
         </a>
       </div>
-      <div className="sideContent">
-        {curContext}
-      </div>
+      <div className='sideContent'>{curContext}</div>
     </div>
   );
-}
+};
 
 export default NavBar;
