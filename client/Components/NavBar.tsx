db88a52187f8943df5e1e5c101cce9019c1c80da
Matt Meigs <mattmeigs@gmail.com>
2020-09-23 12:21:21 -0400

    a tags again

--- client/Components/NavBar.tsx
+++ client/Components/NavBar.tsx
@@ -27,12 +27,12 @@ const NavBar = (props: any) => {
         </svg>
         <h3 className="navBtnText">Home</h3>
       </button></a>
-      <button className="navBtn" onTouchEnd={() => {setPage('about')}} onClick={() => {setPage('about')}}>
+      <a><button className="navBtn" onTouchEnd={() => {setPage('about')}} onClick={() => {setPage('about')}}>
         <svg width="80%" height="70%" viewBox="0 0 16 16" className="bi bi-person-lines-fill" fill="#EBEBEC" xmlns="http://www.w3.org/2000/svg">
           <path fillRule="evenodd" d="M1 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm7 1.5a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 0 1h-2a.5.5 0 0 1-.5-.5zm-2-3a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1h-4a.5.5 0 0 1-.5-.5zm0-3a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1h-4a.5.5 0 0 1-.5-.5zm2 9a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 0 1h-2a.5.5 0 0 1-.5-.5z"/>
         </svg>
         <h3 className="navBtnText">Team</h3>
-      </button>
+      </button></a>
       <button className="navBtn" onClick={() => {setPage('demo')}} ontouchend={() => {setPage('demo')}}>
         <svg width="80%" height="70%" viewBox="0 0 16 16" className="bi bi-laptop" fill="#EBEBEC" xmlns="http://www.w3.org/2000/svg">
           <path fillRule="evenodd" d="M13.5 3h-11a.5.5 0 0 0-.5.5V11h12V3.5a.5.5 0 0 0-.5-.5zm-11-1A1.5 1.5 0 0 0 1 3.5V12h14V3.5A1.5 1.5 0 0 0 13.5 2h-11z"/>
