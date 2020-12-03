a0b4c26978369d4c993944c8ff216f0edef6eaef
Alonso Garza <alonsogarza@Alonsos-MBP.lan>
2020-12-03 17:14:52 -0600

    Removed unused functions and variables.

--- client/Components/Carousel.jsx
+++ client/Components/Carousel.jsx
@@ -1,20 +1,5 @@
 import { React } from '../../deps.ts';
 
-// console.clear();
-
-const slides = [
-  {
-    title: 'Alonso',
-    subtitle: 'Garza',
-    image: '/static/alonso.jpg',
-  },
-  {
-    title: 'Travis',
-    subtitle: 'Frank',
-    image: '/static/travis.png',
-  },
-];
-
 function useTilt(active = true) {
   const ref = React.useRef(null);
 
@@ -55,47 +40,6 @@ function useTilt(active = true) {
   }, [active]);
 
   return ref;
-}
-
-const initialState = {
-  slideIndex: 0,
-};
-
-function Slide({ slide, offset }) {
-  const active = offset === 0 ? true : null;
-  const ref = useTilt(active);
-
-  return (
-    <div
-      ref={ref}
-      className='slide'
-      data-active={active}
-      // style={{
-      //   '--offset': offset,
-      //   '--dir': offset === 0 ? 0 : offset > 0 ? 1 : -1,
-
-      // }}
-    >
-      <div
-        className='slideBackground'
-        // style={{
-        //   backgroundImage: `url('${slide.image}')`,
-        // }}
-      />
-      <div
-        className='slideContent'
-        // style={{
-        //   backgroundImage: `url('${slide.image}')`,
-        // }}
-      >
-        <div className='slideContentInner'>
-          <h2 className='slideTitle'>{slide.title}</h2>
-          <h3 className='slideSubtitle'>{slide.subtitle}</h3>
-          <p className='slideDescription'>{slide.description}</p>
-        </div>
-      </div>
-    </div>
-  );
 }
 
 function Carousel({ user, index, changeUser }) {
