45affac674ea142f38535edc9edb8b9e26ccf105
Alonso Garza <alonsogarza@Alonsos-MacBook-Pro.local>
2020-12-05 17:57:59 -0600

    Test: Returned React verion to 17.0.0

--- deps.ts
+++ deps.ts
@@ -1,6 +1,6 @@
-import React from 'https://dev.jspm.io/react@16.13.1';
-import ReactDomServer from 'https://dev.jspm.io/react-dom@16.13.1/server';
-import ReactDom from 'https://dev.jspm.io/react-dom@16.13.1';
+import React from 'https://dev.jspm.io/react@17.0.0';
+import ReactDomServer from 'https://dev.jspm.io/react-dom@17.0.0/server';
+import ReactDom from 'https://dev.jspm.io/react-dom@17.0.0';
 // import {
 //   ObsidianWrapper,
 //   useObsidian,
