c57e3570ca9d2d1473af1d95f13894f079d63f0d
Matt Meigs <mattmeigs@gmail.com>
2020-12-06 23:29:42 -0500

    new superoak version

--- deno.test.ts
+++ deno.test.ts
@@ -1,16 +1,16 @@
-// import { superoak } from 'https://deno.land/x/superoak@2.1.0/mod.ts';
-// import { describe, it } from 'https://deno.land/x/superoak@2.1.0/test/utils.ts';
-// import { expect } from 'https://deno.land/x/superoak@2.1.0/test/deps.ts';
-// import { app } from './server.tsx';
+import { superoak } from 'https://deno.land/x/superoak@2.3.1/mod.ts';
+import { describe, it } from 'https://deno.land/x/superoak@2.3.1/test/utils.ts';
+import { expect } from 'https://deno.land/x/superoak@2.3.1/test/deps.ts';
+import { app } from './server.tsx';
 
-// describe('GET request to root url', () => {
-//   it('Sends 200 Status and Content Type text/html', async (done) => {
-//     (await superoak(app)).get('/').end((err, res) => {
-//       expect(res.status).toEqual(200);
-//       expect(res.type).toEqual('text/html');
-//       done();
-//     });
-//   });
-// });
+describe('GET request to root url', () => {
+  it('Sends 200 Status and Content Type text/html', async (done) => {
+    (await superoak(app)).get('/').end((err, res) => {
+      expect(res.status).toEqual(200);
+      expect(res.type).toEqual('text/html');
+      done();
+    });
+  });
+});
 
 // deno test -A --unstable
