13d57ffb83c18f45cff3579e5b47cfe7386b53ed
Patrick Sullivan <patrick@jsullivan.org>
2021-06-29 12:37:55 -0700

    Changed Deno runtime bundle emitter syntax to reflect changes with new version of Deno 1.11.3
    Co-authored-by: justinwmckay <justinmckay99@gmail.com>
    Co-authored-by: kyunglee1 <kyunglee3@yahoo.com>
    Co-authored-by: raymondcodes <ray.ahn@gmail.com>
    Co-authored-by: cssim22 <cssim22@gmail.com>

--- server.tsx
+++ server.tsx
@@ -52,13 +52,16 @@ const router = new Router();
 router.get('/', handlePage);
 
 // Bundle the client-side code
-const [_, clientJS] = await Deno.bundle('./client/client.tsx');
+// const [_, clientJS] = await Deno.bundle('./client/client.tsx');
+
+const {files, diagnostics } = await Deno.emit('./client/client.tsx', {bundle: 'module'});
 
 // Router for bundle
 const serverrouter = new Router();
 serverrouter.get('/static/client.js', (context) => {
   context.response.headers.set('Content-Type', 'text/html');
-  context.response.body = clientJS;
+  // context.response.body = clientJS;
+  context.response.body = files['deno:///bundle.js'];
 });
 
 // Implement the routes on the server
